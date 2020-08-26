02625effd217791c8e64dc78c0a15f31bc0d7102
Benjamin Corde Kirton <bkirton@benjamins-mbp-3.lan>
2020-08-26 12:24:57 -0500

    added circle marker over LA

--- mapping_single_points/static/js/logic.js
+++ mapping_single_points/static/js/logic.js
@@ -12,12 +12,21 @@ console.log("working");
 
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+//add a marker to the map for LA, CA
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//add a circle marker
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor:'#ffffa1'
+}).addTo(map);
 
 // create tile layer that will be background of our map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
